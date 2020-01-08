7fd4b62d1fe4c2b2e3310b03e030620df8bb96bc
Dmitry Ischenko <ischenko.dmitry@gmail.com>
2020-01-09 01:16:22 +0300

    Rename layers and add horovod optimizers

---
+++ init.py
@@ -0,0 +1,70 @@
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from datetime import datetime
+from matplotlib import pyplot as plt
+import os
+
+from dataset.dataset_class import VidDataSet
+from dataset.video_extraction_conversion import *
+from loss.loss_discriminator import *
+from loss.loss_generator import *
+from network.blocks import *
+from network.model import *
+
+
+"""Create dataset and net"""
+device = torch.device("cuda:0")
+cpu = torch.device("cpu")
+
+path_to_chkpt = 'model_weights.tar'
+path_to_backup = 'backup_model_weights.tar'
+
+dataset = VidDataSet(K=8, path_to_mp4 = '/home/vera/data/voxceleb2/tmp/mp4', device=device)
+
+dataLoader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+G = Generator(224).to(device)
+E = Embedder(224).to(device)
+D = Discriminator(dataset.__len__()).to(device)
+
+G.train()
+E.train()
+D.train()
+
+optimizerG = optim.Adam(params = G.parameters(), lr=5e-5)
+optimizerE = optim.Adam(params = E.parameters(), lr=5e-5)
+optimizerD = optim.Adam(params = D.parameters(), lr=2e-4)
+
+"""Criterion"""
+criterionG = LossG(VGGFace_body_path='Pytorch_VGGFACE_IR.py',
+                   VGGFace_weight_path='Pytorch_VGGFACE.pth', device=device)
+criterionDreal = LossDSCreal()
+criterionDfake = LossDSCfake()
+
+
+"""Training init"""
+epochCurrent = epoch = i_batch = 0
+lossesG = []
+lossesD = []
+i_batch_current = 0
+
+num_epochs = 750
+
+#initiate checkpoint if inexistant
+if not os.path.isfile(path_to_chkpt):
+  print('Initiating new checkpoint...')
+  torch.save({
+          'epoch': epoch,
+          'lossesG': lossesG,
+          'lossesD': lossesD,
+          'E_state_dict': E.state_dict(),
+          'G_state_dict': G.state_dict(),
+          'D_state_dict': D.state_dict(),
+          'optimizerG_state_dict': optimizerG.state_dict(),
+          'optimizerE_state_dict': optimizerE.state_dict(),
+          'optimizerD_state_dict': optimizerD.state_dict(),
+          'num_vid': dataset.__len__(),
+          'i_batch': i_batch
+          }, path_to_chkpt)
+  print('...Done')
--- train.py
+++ train.py
@@ -13,14 +13,24 @@ from loss.loss_generator import *
 from network.blocks import *
 from network.model import *
 
+import horovod.torch as hvd
+
+hvd.init()
+
+torch.cuda.set_device(hvd.local_rank())
+
+
+
 """Create dataset and net"""
-device = torch.device("cuda:0")
+device = torch.device("cuda")
 cpu = torch.device("cpu")
+
 path_to_chkpt = 'model_weights.tar'
 path_to_backup = 'backup_model_weights.tar'
-dataset = VidDataSet(K=8, path_to_mp4 = 'mp4', device=device)
 
-dataLoader = DataLoader(dataset, batch_size=2, shuffle=True)
+dataset = VidDataSet(K=8, path_to_mp4 = '/home/vera/data/voxceleb2/tmp/mp4', device=device)
+
+dataLoader = DataLoader(dataset, batch_size=1, shuffle=True)
 
 G = Generator(224).to(device)
 E = Embedder(224).to(device)
@@ -30,9 +40,19 @@ G.train()
 E.train()
 D.train()
 
-optimizerG = optim.Adam(params = list(E.parameters()) + list(G.parameters()), lr=5e-5)
+optimizerG = optim.Adam(params = G.parameters(), lr=5e-5)
+optimizerE = optim.Adam(params = E.parameters(), lr=5e-5)
 optimizerD = optim.Adam(params = D.parameters(), lr=2e-4)
 
+optimizerG = hvd.DistributedOptimizer(optimizerG, named_parameters=G.named_parameters())
+optimizerE = hvd.DistributedOptimizer(optimizerE, named_parameters=E.named_parameters())
+optimizerD = hvd.DistributedOptimizer(optimizerD, named_parameters=D.named_parameters())
+
+
+# Broadcast parameters from rank 0 to all other processes.
+hvd.broadcast_parameters(G.state_dict(), root_rank=0)
+hvd.broadcast_parameters(E.state_dict(), root_rank=0)
+hvd.broadcast_parameters(D.state_dict(), root_rank=0)
 
 """Criterion"""
 criterionG = LossG(VGGFace_body_path='Pytorch_VGGFACE_IR.py',
@@ -49,30 +69,18 @@ i_batch_current = 0
 
 num_epochs = 750
 
-#initiate checkpoint if inexistant
-if not os.path.isfile(path_to_chkpt):
-  print('Initiating new checkpoint...')
-  torch.save({
-          'epoch': epoch,
-          'lossesG': lossesG,
-          'lossesD': lossesD,
-          'E_state_dict': E.state_dict(),
-          'G_state_dict': G.state_dict(),
-          'D_state_dict': D.state_dict(),
-          'optimizerG_state_dict': optimizerG.state_dict(),
-          'optimizerD_state_dict': optimizerD.state_dict(),
-          'num_vid': dataset.__len__(),
-          'i_batch': i_batch
-          }, path_to_chkpt)
-  print('...Done')
+#initiate checkpoint if inexistant in init.py
 
 """Loading from past checkpoint"""
 checkpoint = torch.load(path_to_chkpt, map_location=cpu)
 E.load_state_dict(checkpoint['E_state_dict'])
 G.load_state_dict(checkpoint['G_state_dict'])
 D.load_state_dict(checkpoint['D_state_dict'])
+
 optimizerG.load_state_dict(checkpoint['optimizerG_state_dict'])
+optimizerE.load_state_dict(checkpoint['optimizerE_state_dict'])
 optimizerD.load_state_dict(checkpoint['optimizerD_state_dict'])
+
 epochCurrent = checkpoint['epoch']
 lossesG = checkpoint['lossesG']
 lossesD = checkpoint['lossesD']
@@ -95,12 +103,13 @@ for epoch in range(epochCurrent, num_epochs):
         with torch.autograd.enable_grad():
             #zero the parameter gradients
             optimizerG.zero_grad()
+            optimizerE.zero_grad()
             optimizerD.zero_grad()
 
             #forward
             # Calculate average encoding vector for video
             f_lm_compact = f_lm.view(-1, f_lm.shape[-4], f_lm.shape[-3], f_lm.shape[-2], f_lm.shape[-1]) #BxK,2,3,224,224
-            
+
             e_vectors = E(f_lm_compact[:,0,:,:,:], f_lm_compact[:,1,:,:,:]) #BxK,512,1
             e_vectors = e_vectors.view(-1, f_lm.shape[1], 512, 1) #B,K,512,1
             e_hat = e_vectors.mean(dim=1)
@@ -118,11 +127,13 @@ for epoch in range(epochCurrent, num_epochs):
             loss = lossDreal + lossDfake + lossG
             loss.backward(retain_graph=False)
             optimizerG.step()
+            optimizerE.step()
             optimizerD.step()
-            
-            
+
+
             #train D again
             optimizerG.zero_grad()
+            optimizerE.zero_grad()
             optimizerD.zero_grad()
             x_hat.detach_()
             r_hat, D_hat_res_list = D(x_hat, g_y, i)
@@ -136,14 +147,15 @@ for epoch in range(epochCurrent, num_epochs):
             optimizerD.step()
 
 
+        """
         # Output training stats
-        if i_batch % 10 == 0:
+        if i_batch % 10 == 0 and hvd.local_rank() == 0:
             batch_end = datetime.now()
             avg_time = (batch_end - batch_start) / 10
             print('\n\navg batch time for batch size of', x.shape[0],':',avg_time)
-            
+
             batch_start = datetime.now()
-            
+
             print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(y)): %.4f'
                   % (epoch, num_epochs, i_batch, len(dataLoader),
                      lossD.item(), lossG.item(), r.mean(), r_hat.mean()))
@@ -171,8 +183,9 @@ for epoch in range(epochCurrent, num_epochs):
             out = out.type(torch.int32).to(cpu).numpy()
             plt.imshow(out)
             plt.show()
+        """
 
-        if i_batch % 100 == 99:
+        if i_batch % 100 == 99 and hvd.local_rank() == 0:
             lossesD.append(lossD.item())
             lossesG.append(lossG.item())
 
@@ -190,13 +203,14 @@ for epoch in range(epochCurrent, num_epochs):
                     'G_state_dict': G.state_dict(),
                     'D_state_dict': D.state_dict(),
                     'optimizerG_state_dict': optimizerG.state_dict(),
+                    'optimizerE_state_dict': optimizerE.state_dict(),
                     'optimizerD_state_dict': optimizerD.state_dict(),
                     'num_vid': dataset.__len__(),
                     'i_batch': i_batch
                     }, path_to_chkpt)
             print('...Done saving latest')
-        
-        if i_batch % 500 == 499:
+
+        if i_batch % 500 == 499 and hvd.local_rank() == 0:
 
             print('Saving latest...')
             torch.save({
@@ -207,10 +221,9 @@ for epoch in range(epochCurrent, num_epochs):
                     'G_state_dict': G.state_dict(),
                     'D_state_dict': D.state_dict(),
                     'optimizerG_state_dict': optimizerG.state_dict(),
+                    'optimizerE_state_dict': optimizerE.state_dict(),
                     'optimizerD_state_dict': optimizerD.state_dict(),
                     'num_vid': dataset.__len__(),
                     'i_batch': i_batch
                     }, path_to_backup)
             print('...Done saving latest')
-
-
